ba06da9ee64e322c3c0f1eec73319b5318318875
nashiludi <92453023+nashiludi@users.noreply.github.com>
2021-11-17 23:32:15 +0200

    Delete nashiboostv2 directory

--- nashiboostv2/js/main.js
+++
@@ -1,8 +0,0 @@
-let bg = document.querySelectorAll('.parallax_circle');
-for (let i = 0; i < bg.length; i++){
-    window.addEventListener('mousemove', function(e) { 
-        let x = e.clientX / window.innerWidth;
-        let y = e.clientY / window.innerHeight;     
-        bg[i].style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
-    });    
-}
